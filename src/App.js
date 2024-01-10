53b801b31246c27584aeee96a7a2d7cc0070be31
Aain Ur Rehman <aain134@gmail.com>
2024-01-10 11:26:12 +0500

    Add sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Topbar from './components/topbar/Topbar'
-
+import Sidebar from './components/sidebar/Sidebar'
+import'./App.css'
 function App() {
   return (
     <div>
 <Topbar/>
+<div className='container'>
+<Sidebar/>
+<div className='others'>
+  other pages
+</div>
+</div>
     </div>
   )
 }
