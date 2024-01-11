c31d8765dca7aa9e753bbc6e827dcf61350fa4b5
Aain Ur Rehman <aain134@gmail.com>
2024-01-11 11:32:06 +0500

    Add UserList

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import Topbar from './components/topbar/Topbar'
-import Sidebar from './components/sidebar/Sidebar'
-import'./App.css'
-import Home from './pages/home/Home'
+import React from 'react';
+import Topbar from './components/topbar/Topbar';
+import Sidebar from './components/sidebar/Sidebar';
+import './App.css';
+import Home from './pages/home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
+
 function App() {
   return (
-    <div>
-<Topbar/>
-<div className='container'>
-<Sidebar/>
-<Home/>
-
-</div>
-    </div>
-  )
+    <Router>
+      <Topbar />
+      <div className='container'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/users' element={<UserList />} />
+          <Route path='/user/user:Id' element={<User />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/pages/user/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './User.css'
+function User() {
+  return (
+    <div className='user'>
+
+    </div>
+  )
+}
+
+export default User
