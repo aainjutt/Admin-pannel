11fad2ddfcbe14ba6efe6679c24ba99f2c2ca4d8
Aain Ur Rehman <aain134@gmail.com>
2024-01-11 14:40:03 +0500

    Add User

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import Topbar from './components/topbar/Topbar';
 import Sidebar from './components/sidebar/Sidebar';
 import './App.css';
 import Home from './pages/home/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router, // Changed to BrowserRouter as Router
+  Routes,
+  Route,
+} from "react-router-dom";
 import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
+
 
 function App() {
   return (
@@ -14,9 +19,10 @@ function App() {
       <div className='container'>
         <Sidebar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/users' element={<UserList />} />
-          <Route path='/user/user:Id' element={<User />} />
+        <Route path="/" element={<Home />} /> {/* Replaced Route syntax */}
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
+        
         </Routes>
       </div>
     </Router>
